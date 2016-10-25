0f216bf829cc60a4dc82fd71ba594c4ea5020aef
AwokeKnowing <jamesdavidmorris@gmail.com>
2016-10-24 21:34:48 -0700

    1 step setup

--- setup.sh
+++ setup.sh
@@ -6,7 +6,37 @@
 
 #add vtt files as text files
 #docker exec -it --user=solr my_solr bin/post -c gettingstarted  mydata/ -filetypes vtt -type text/plain
-
+echo
+echo "--------------------------------------------------"
+echo "Stopping / removing any previous instance"
 docker stop kcsolr
 docker rm -v kcsolr
+echo
+echo "Starting solr docker image with core: knowledgecity"
 docker run --name kcsolr -d -p 8983:8983 solr solr-create -c knowledgecity
+echo
+
+
+printf "Waiting for solr"
+until $(curl --output /dev/null --silent --head --fail http://localhost:8983); do
+    printf '.'
+    sleep 3
+done
+
+echo
+printf "Waiting for core: knowledgecity"
+until [[ $(curl -sS http://localhost:8983/solr/admin/cores?action=STATUS) =~ knowledgecity ]]; do
+    printf '.'
+    sleep 1
+done	
+echo
+
+
+echo "Adding lessons schema"
+bash addschema-lessons.sh
+echo 
+echo "Indexing test data"
+bash test-lessons.sh
+echo
+echo "Setup complete!"
+echo "--------------------------------------------------"
