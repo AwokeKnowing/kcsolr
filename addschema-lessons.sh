38cd8c171cb26c9db01f626fb3ba20f19c62b95b
AwokeKnowing <jamesdavidmorris@gmail.com>
2016-10-24 20:28:17 -0700

    rename allSearchText

--- addschema-lessons.sh
+++ addschema-lessons.sh
@@ -1,21 +1,21 @@
 curl -X POST -H 'Content-type:application/json' --data-binary '{
   "add-field":[
-    { "name":"vttContent",    "type":"text_en", "stored":true },
-    { "name":"vttPureText",   "type":"text_en", "stored":true },
-    { "name":"courseId",      "type":"text_en", "stored":true },
-    { "name":"lessonId",      "type":"text_en", "stored":true },
-    { "name":"languageCode",  "type":"text_en", "stored":true },
-    { "name":"lessonTitle",   "type":"text_en", "stored":true },
-    { "name":"posterImage",   "type":"text_en", "stored":true },
-    { "name":"alltext",       "type":"text_en", "stored":true, "multiValued":true }
+    { "name": "vttContent",        "type": "text_en", "stored": true },
+    { "name": "vttPureText",       "type": "text_en", "stored": true },
+    { "name": "courseId",          "type": "text_en", "stored": true },
+    { "name": "lessonId",          "type": "text_en", "stored": true },
+    { "name": "languageCode",      "type": "text_en", "stored": true },
+    { "name": "lessonTitle",       "type": "text_en", "stored": true },
+    { "name": "posterImage",       "type": "text_en", "stored": true },
+    { "name": "allSearchText",     "type": "text_en", "stored": true, "multiValued": true }
   ],
   "add-copy-field":[
-    { "source":"vttContent",  "dest":["alltext"] },   
-    { "source":"vttPureText", "dest":["alltext"] },
-    { "source":"courseId",    "dest":["alltext"] },
-    { "source":"lessonId",   "dest":["alltext"] },
-    { "source":"languageCode","dest":["alltext"] },
-    { "source":"lessonTitle", "dest":["alltext"] },
-    { "source":"posterImage", "dest":["alltext"] }
+    { "source": "vttContent",      "dest": ["allSearchText"] },   
+    { "source": "vttPureText",     "dest": ["allSearchText"] },
+    { "source": "courseId",        "dest": ["allSearchText"] },
+    { "source": "lessonId",        "dest": ["allSearchText"] },
+    { "source": "languageCode",    "dest": ["allSearchText"] },
+    { "source": "lessonTitle",     "dest": ["allSearchText"] },
+    { "source": "posterImage",     "dest": ["allSearchText"] }
   ]
 }' http://localhost:8983/solr/knowledgecity/schema
