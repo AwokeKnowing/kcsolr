1a817344ff8eebd1701b0bbd9d304622394c70e8
AwokeKnowing <jamesdavidmorris@gmail.com>
2016-10-25 23:58:01 -0700

    repush

--- vtt2text.sh
+++ vtt2text.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 fname=$1
-#sed 's/\r$//' "$fname" | awk -vRS= -vORS= -F'\n' '{for (i=3;i<=NF;i++) print $i} END {print "\n"}'
+#sed 's/\r$//' "$fname" | awk -vRS= -vORS= -F'\n' '{for (i=3;i<=NF;i++) print $i} END {print "\n"}' 
 sed 's/\r$//' "$fname"    |\
 grep -v -- "-->"          |\
 grep -v "^$"              |\
