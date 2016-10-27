63f5421b051b2e688ee5fb75558714ddf377d837
AwokeKnowing <jamesdavidmorris@gmail.com>
2016-10-26 23:45:14 -0700

    use local vtt

--- cdnvtt2json.sh
+++ cdnvtt2json.sh
@@ -3,8 +3,9 @@ language=$1
 courseid=$2
 lesson=$3
 
-vttpuretext=$(curl 'https://cdn0.knowledgecity.com/opencontent/courses/captions/vtt/'$language'/'$courseid'-'$lesson'.vtt' 2>/dev/null | bash vtt2text.sh)
+#vttpuretext=$(curl 'https://cdn0.knowledgecity.com/opencontent/courses/captions/vtt/'$language'/'$courseid'-'$lesson'.vtt' 2>/dev/null | bash vtt2text.sh)
 #vttpuretext=$(curl 'file://'$(pwd)'/test.vtt' 2>/dev/null | bash vtt2text.sh)
+vttpuretext=$(cat '../../solrdata/knowledgecity/vtt/'$language'/'$courseid'-'$lesson'.vtt' | bash vtt2text.sh)
 
 echo '{"courseId": "'$courseid'",'
 echo ' "lessonId": "'$courseid'-'$lesson'",'
