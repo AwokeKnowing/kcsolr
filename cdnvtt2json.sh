10b19e659d02f620ca8de2b92bca953a05b4e162
awokeknowing <jamesdavidmorris@gmail.com>
2017-01-18 10:17:36 -0800

    make default to pull from cdn instead of local

--- cdnvtt2json.sh
+++ cdnvtt2json.sh
@@ -3,9 +3,11 @@ language=$1
 courseid=$2
 lesson=$3
 
-#vttpuretext=$(curl 'https://cdn0.knowledgecity.com/opencontent/courses/captions/vtt/'$language'/'$courseid'-'$lesson'.vtt' 2>/dev/null | bash vtt2text.sh)
+vttpuretext=$(curl 'https://cdn0.knowledgecity.com/opencontent/courses/captions/vtt/'$language'/'$courseid'-'$lesson'.vtt' 2>/dev/null | bash vtt2text.sh)
+
+#you can also pull from local copy of vtt files which is at least 10x faster
 #vttpuretext=$(curl 'file://'$(pwd)'/test.vtt' 2>/dev/null | bash vtt2text.sh)
-vttpuretext=$(cat '../../solrdata/knowledgecity/vtt/'$language'/'$courseid'-'$lesson'.vtt' | bash vtt2text.sh)
+#vttpuretext=$(cat '../../solrdata/knowledgecity/vtt/'$language'/'$courseid'-'$lesson'.vtt' | bash vtt2text.sh)
 
 echo '{"courseId": "'$courseid'",'
 echo ' "lessonId": "'$courseid'-'$lesson'",'
